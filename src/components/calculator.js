9737a26f1154d86c8841c1727062ebb6d70dcf6d
RashidMenhas <rashidmenhas10@gmail.com>
2022-11-09 23:49:05 +0500

    Create Basic UI of Math-Magician Application

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,25 +5,25 @@ class Calculator extends React.PureComponent {
   render() {
     return (
       <div className="container">
-        <div className="result-area">0</div>
+        <div className="result-area"><p className="result">0</p></div>
 
         <div className="All-btns">
           <button type="button" className="AC">AC</button>
           <button type="button" className="PM">+/-</button>
           <button type="button" className="modules">%</button>
-          <button type="button" className="div">÷</button>
+          <button type="button" className="div gold">÷</button>
           <button type="button" className="seven">7</button>
           <button type="button" className="eigth">8</button>
           <button type="button" className="nine">9</button>
-          <button type="button" className="x">x</button>
+          <button type="button" className="x gold">x</button>
           <button type="button" className="four">4</button>
           <button type="button" className="five">5</button>
           <button type="button" className="six">6</button>
-          <button type="button" className="minus">-</button>
+          <button type="button" className="minus gold">-</button>
           <button type="button" className="one">1</button>
           <button type="button" className="two">2</button>
           <button type="button" className="three">3</button>
-          <button type="button" className="plus">+</button>
+          <button type="button" className="plus gold">+</button>
           <button type="button" className="zero">0</button>
           <button type="button" className="dot">.</button>
           <button type="button" className="equal">=</button>
