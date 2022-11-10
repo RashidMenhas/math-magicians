cd1d0a180fb62826681cef0c5a2e67da3412f5b9
RashidMenhas <rashidmenhas10@gmail.com>
2022-11-11 00:26:59 +0500

    Use Hooks in a React

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,56 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { total: 0, next: null, operation: null };
-    this.handlerevent = this.handlerevent.bind(this);
+function Calculator() {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
+
+  const handlerevent = (event) => {
+    const { target: { textContent: buttonName } } = event;
+    const result = calculate(state, buttonName);
+    setState(result);
+  };
+
+  const { total, next, operation } = state;
+  let result = '';
+  if (total) {
+    result = `${total} ${operation || ''} ${next || ''}`;
+  } else if (next) {
+    result = `${next || ''} ${operation || ''}`;
   }
+  return (
+    <div className="container">
+      <div className="result-area"><p className="result">{result || 0}</p></div>
 
-     handlerevent= (event) => {
-       const { target: { textContent: buttonName } } = event;
-       const result = calculate(this.state, buttonName);
-       this.setState(result);
-     }
+      <div className="All-btns">
+        <button type="button" className="AC" onClick={handlerevent}>AC</button>
+        <button type="button" className="PM" onClick={handlerevent}>+/-</button>
+        <button type="button" className="modules" onClick={handlerevent}>%</button>
+        <button type="button" className="div gold" onClick={handlerevent}>÷</button>
+        <button type="button" className="seven" onClick={handlerevent}>7</button>
+        <button type="button" className="eigth" onClick={handlerevent}>8</button>
+        <button type="button" className="nine" onClick={handlerevent}>9</button>
+        <button type="button" className="x gold" onClick={handlerevent}>x</button>
+        <button type="button" className="four" onClick={handlerevent}>4</button>
+        <button type="button" className="five" onClick={handlerevent}>5</button>
+        <button type="button" className="six" onClick={handlerevent}>6</button>
+        <button type="button" className="minus gold" onClick={handlerevent}>-</button>
+        <button type="button" className="one" onClick={handlerevent}>1</button>
+        <button type="button" className="two" onClick={handlerevent}>2</button>
+        <button type="button" className="three" onClick={handlerevent}>3</button>
+        <button type="button" className="plus gold" onClick={handlerevent}>+</button>
+        <button type="button" className="zero" onClick={handlerevent}>0</button>
+        <button type="button" className="dot" onClick={handlerevent}>.</button>
+        <button type="button" className="equal gold" onClick={handlerevent}>=</button>
 
-     render() {
-       const { total, next, operation } = this.state;
-       let result = '';
-       if (total) {
-         result = `${total} ${operation || ''} ${next || ''}`;
-       } else if (next) {
-         result = `${next || ''} ${operation || ''}`;
-       }
-       return (
-         <div className="container">
-           <div className="result-area"><p className="result">{result || 0}</p></div>
-
-           <div className="All-btns">
-             <button type="button" className="AC" onClick={this.handlerevent}>AC</button>
-             <button type="button" className="PM" onClick={this.handlerevent}>+/-</button>
-             <button type="button" className="modules" onClick={this.handlerevent}>%</button>
-             <button type="button" className="div gold" onClick={this.handlerevent}>÷</button>
-             <button type="button" className="seven" onClick={this.handlerevent}>7</button>
-             <button type="button" className="eigth" onClick={this.handlerevent}>8</button>
-             <button type="button" className="nine" onClick={this.handlerevent}>9</button>
-             <button type="button" className="x gold" onClick={this.handlerevent}>x</button>
-             <button type="button" className="four" onClick={this.handlerevent}>4</button>
-             <button type="button" className="five" onClick={this.handlerevent}>5</button>
-             <button type="button" className="six" onClick={this.handlerevent}>6</button>
-             <button type="button" className="minus gold" onClick={this.handlerevent}>-</button>
-             <button type="button" className="one" onClick={this.handlerevent}>1</button>
-             <button type="button" className="two" onClick={this.handlerevent}>2</button>
-             <button type="button" className="three" onClick={this.handlerevent}>3</button>
-             <button type="button" className="plus gold" onClick={this.handlerevent}>+</button>
-             <button type="button" className="zero" onClick={this.handlerevent}>0</button>
-             <button type="button" className="dot" onClick={this.handlerevent}>.</button>
-             <button type="button" className="equal gold" onClick={this.handlerevent}>=</button>
-
-           </div>
-         </div>
-       );
-     }
+      </div>
+    </div>
+  );
 }
+
 export default Calculator;
